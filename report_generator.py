b4739788357738127493406746cab70686d2e5c3
lesliemilethgonzalezperez1009-prog <lesliemilethgonzalezperez1009@gmail.com>
2026-08-30 18:42:01 -0500

    ahora la tabla esta en excel

--- report_generator.py
+++ report_generator.py
@@ -1,15 +1,21 @@
 import csv
 
+from openpyxl import Workbook
+from openpyxl.styles import Font
+
+book = Workbook()
+sheet = book.active
+
+sheet.merge_cells('A1:D1')
+sheet['A1'] = 'ANALISIS DE DATOS CSV'
+
+
 with open('ventas.csv', 'r') as cvs_file:
     cvs_reader = csv.reader(cvs_file)
     next(cvs_reader)
 
     for line in cvs_reader:
         print(f"Producto: {line[0]} | Categoria: {line[1]} | Unidades: {line[2]} | Precio unitario: {line[3]} ")
+        sheet.append([line[0], line[1], line[2], line[3]]) 
 
-  #  with open ('DATA', mode = 'w', newline= "",encoding = 'UTF-8') as file:
-   #     csv_writer = csv.writer(file ,delimiter= "|")
-#
- ##          csv_writer.writerow(line)
- # print ('TODO LISTO')
-        
+book.save('Reporte.xlsx')
